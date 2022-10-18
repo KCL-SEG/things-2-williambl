a358c98d2f798d455f8d8efda03940c4ec38e424
williambl <william@williambl.com>
2022-10-18 12:31:47 +0100

    feat: add things form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,12 @@
 """Forms of the project."""
+from django import forms
+
+from things.models import Thing
+
 
 # Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {'description': forms.Textarea()}
--- things/views.py
+++ things/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
+from things.forms import ThingForm
+
+
 def home(request):
+    form = ThingForm()
     return render(request, 'home.html', {'form': form})
